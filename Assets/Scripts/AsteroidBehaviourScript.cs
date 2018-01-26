9cf89a09d7058da22cd7221dbb2299331124481f
Esa Karjalainen <esa.karjalainen@gmail.com>
2018-01-26 22:08:46 +0200

    new branch because running out of battery

---
+++ Assets/Scripts/AsteroidBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviourScript : MonoBehaviour {
+    Vector3 v3_down = new Vector3(0.0f, -0.05f);
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(v3_down);
+	}
+}
