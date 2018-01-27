92394740669fa4c3a6c3ffb3b3a4f72cc460ed75
Esa Karjalainen <esa.karjalainen@gmail.com>
2018-01-27 12:52:14 +0200

    asteroid, vanish!

--- Assets/Scripts/AsteroidBehaviourScript.cs
+++ Assets/Scripts/AsteroidBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AsteroidBehaviourScript : MonoBehaviour {
     Vector3 v3_down = new Vector3(0.0f, -0.05f);
+    //Vector3 v3_scaleAway = new Vector3(0.5f, 0.5f, 1f);
 
     // Use this for initialization
     void Start () {
@@ -13,6 +14,13 @@ public class AsteroidBehaviourScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.transform.Translate(v3_down, Space.World);
+        if (this.transform.position.y < -1) {
+            Debug.Log("scale away");
+            var s = this.transform.localScale;
+            this.transform.localScale = new Vector3(s.x / 1.1f, s.y / 1.1f, s.z);
+            var p = this.transform.position;
+            this.transform.position = new Vector3(p.x / 1.05f, p.y, p.z);
+        }
 	}
 
     private void OnBecameInvisible()
