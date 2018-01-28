01fa072baa91c2de183a6f84fe2488393892c45a
Esa Karjalainen <esa.karjalainen@gmail.com>
2018-01-28 12:49:47 +0200

    llamas in right spot

--- Assets/Scripts/AsteroidSpawnerBehaviorScript.cs
+++ Assets/Scripts/AsteroidSpawnerBehaviorScript.cs
@@ -17,7 +17,7 @@ public class AsteroidSpawnerBehaviorScript : MonoBehaviour {
    }
 
     private float lastSpawnTime = 0;
-    private float spawnDelay = 2f;
+    private float spawnDelay = 4f;
 
 	// Update is called once per frame
 	void Update () {
@@ -41,17 +41,22 @@ public class AsteroidSpawnerBehaviorScript : MonoBehaviour {
         if (Time.time - lastSpawnTime > spawnDelay && AsteroidPrefabs.Length > 0)
         {
             Debug.Log("Camera size: " + size);
-            lastSpawnTime = Time.time;
+            lastSpawnTime = Time.time - Random.value;
             Debug.Log("spawn new object");
             //var scene = SceneManager.GetActiveScene();
-            int index = Mathf.RoundToInt(Random.value * (AsteroidPrefabs.Length - 1));
+            /*int index = Mathf.RoundToInt(Random.value * (AsteroidPrefabs.Length));
+            if (index == AsteroidPrefabs.Length) {
+                index = AsteroidPrefabs.Length - 1;
+            }*/
+            int index = Random.Range(0, AsteroidPrefabs.Length);
+
             if (index > delay*2.5f) {
                 index = Mathf.RoundToInt(delay*2.5f);
             }
 
             Debug.Log(index);
             var asteroid = Instantiate(AsteroidPrefabs[index], gameObject.transform);
-            asteroid.transform.Translate(-size / aspect + Random.value * 2 * size / aspect, 0, 10);
+            asteroid.transform.Translate( (-size / aspect) + (Random.value * 2 * size / aspect), 0, 10);
             asteroid.transform.Rotate(Vector3.back, Random.value * 360);
             asteroid.GetComponent<AsteroidBehaviourScript>().rotationSpeed = (-1f + Random.value * 2f) * (1f / (1+Mathf.Pow(index, 2)) );
              
